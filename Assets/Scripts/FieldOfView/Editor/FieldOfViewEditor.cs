3d99913560c959c7a88c5ae953d2b8875b2639ce
Milly <milabits@gmail.com>
2019-02-26 00:10:00 +0100

    Wall detection working, stencil shaders not working

---
+++ Assets/Scripts/FieldOfView/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,60 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+    private FieldOfView fov;
+
+
+    private void OnEnable()
+    {
+        fov = (FieldOfView) target;
+    }
+
+    private void OnSceneGUI()
+    {
+        if (EditorApplication.isPlaying) return;
+        
+        GUIStyle labelStyle = new GUIStyle {fontSize = 14};
+        var viewPoints = fov.CalculateViewPoints();
+
+        Handles.color = Color.gray;
+        labelStyle.normal.textColor = Color.yellow;
+        Handles.Label(viewPoints[0] - fov.DetectionOffset, "Visual Cone", labelStyle);
+        Vector3 viewAngleA = fov.DirectionFromAngle(-fov.ViewConeWidth / 2, false);
+        Vector3 viewAngleB = fov.DirectionFromAngle(fov.ViewConeWidth / 2, false);
+        Handles.DrawLine(fov.transform.position,
+            fov.transform.position + viewAngleA * fov.ViewRadius);
+        Handles.DrawLine(fov.transform.position,
+            fov.transform.position + viewAngleB * fov.ViewRadius);
+
+
+        // Draw Visual FOV outline;
+        Handles.color = Color.yellow;
+        for (int i = 1; i < viewPoints.Count; i++)
+        {
+            Handles.DrawLine(viewPoints[i] - fov.DetectionOffset, viewPoints[i - 1] - fov.DetectionOffset);
+        }
+
+
+        Handles.color = Color.gray;
+        labelStyle.normal.textColor = Color.green;
+        Handles.Label(viewPoints[0], "Detection Cone", labelStyle);
+        Handles.DrawWireArc(fov.transform.position + fov.DetectionOffset, Vector3.up, Vector3.forward, 360,
+            fov.ViewRadius);
+
+        Handles.DrawLine(fov.transform.position + fov.DetectionOffset,
+            fov.transform.position + fov.DetectionOffset + viewAngleA * fov.ViewRadius);
+        Handles.DrawLine(fov.transform.position + fov.DetectionOffset,
+            fov.transform.position + fov.DetectionOffset + viewAngleB * fov.ViewRadius);
+
+        // Draw Visual FOV outline;
+        Handles.color = Color.green;
+        for (int i = 1; i < viewPoints.Count; i++)
+        {
+            Handles.DrawLine(viewPoints[i], viewPoints[i - 1]);
+            Handles.DrawLine(viewPoints[i], fov.transform.position + fov.DetectionOffset);
+        }
+    }
+}
---
+++ Assets/Scripts/FieldOfView/FieldOfView.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float ViewRadius;
+
+    [Range(0, 360)]
+    public float ViewConeWidth;
+
+    public LayerMask ObstacleMask;
+    public float MeshResolution;
+    public int EdgeResolveIterations;
+    public float EdgeDistanceThreshold;
+    public Vector3 DetectionOffset;
+
+    public MeshFilter ViewMeshFilter;
+    private Mesh _viewMesh;
+
+    private JobHandle raycastHandle;
+
+    private void Start()
+    {
+        _viewMesh = new Mesh();
+        _viewMesh.name = "View Mesh";
+        ViewMeshFilter.mesh = _viewMesh;
+    }
+
+    private void Update()
+    {
+        DrawFieldOfView();
+    }
+
+    public Vector3 DirectionFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+    public void DrawFieldOfView()
+    {
+        var viewPoints = CalculateViewPoints();
+
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+
+        vertices[0] = Vector3.zero;
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i] - DetectionOffset);
+
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+
+        _viewMesh.Clear();
+        _viewMesh.vertices = vertices;
+        _viewMesh.triangles = triangles;
+        _viewMesh.RecalculateNormals();
+    }
+
+    // TODO: Implement RaycastCommand https://docs.unity3d.com/ScriptReference/RaycastCommand.html
+    public List<Vector3> CalculateViewPoints()
+    {
+        int stepCount = Mathf.RoundToInt(ViewConeWidth * MeshResolution);
+        float stepAngleSize = ViewConeWidth / stepCount;
+
+        List<Vector3> viewPoints = new List<Vector3>();
+        ViewCastInfo oldViewCast = new ViewCastInfo();
+        for (int i = 0; i <= stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - ViewConeWidth / 2 + stepAngleSize * i;
+            ViewCastInfo newViewCast = ViewCast(angle);
+
+            if (i > 0)
+            {
+                bool edgeDistanceThresholdExceeded =
+                    Mathf.Abs(oldViewCast.Distance - newViewCast.Distance) > EdgeDistanceThreshold;
+                if (oldViewCast.Hit != newViewCast.Hit ||
+                    (oldViewCast.Hit && newViewCast.Hit && edgeDistanceThresholdExceeded))
+                {
+                    EdgeInfo edge = FindEdge(oldViewCast, newViewCast);
+                    if (edge.PointA != Vector3.zero) viewPoints.Add(edge.PointA);
+                    if (edge.PointB != Vector3.zero) viewPoints.Add(edge.PointB);
+                }
+            }
+
+            viewPoints.Add(newViewCast.Point);
+            oldViewCast = newViewCast;
+        }
+
+        return viewPoints;
+    }
+
+    EdgeInfo FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast)
+    {
+        float minAngle = minViewCast.Angle;
+        float maxAngle = maxViewCast.Angle;
+        Vector3 minPoint = Vector3.zero;
+        Vector3 maxPoint = Vector3.zero;
+
+        for (int i = 0; i < EdgeResolveIterations; i++)
+        {
+            float angle = (minAngle + maxAngle) / 2;
+            ViewCastInfo newViewCast = ViewCast(angle);
+
+            bool edgeDistanceThresholdExceeded =
+                Mathf.Abs(minViewCast.Distance - newViewCast.Distance) > EdgeDistanceThreshold;
+            if (newViewCast.Hit == minViewCast.Hit && !edgeDistanceThresholdExceeded)
+            {
+                minAngle = angle;
+                minPoint = newViewCast.Point;
+            }
+            else
+            {
+                maxAngle = angle;
+                maxPoint = newViewCast.Point;
+            }
+        }
+
+        return new EdgeInfo(minPoint, maxPoint);
+    }
+
+    ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 dir = DirectionFromAngle(globalAngle, true);
+        RaycastHit hit;
+
+
+        if (Physics.Raycast((transform.position + DetectionOffset), dir, out hit, ViewRadius, ObstacleMask))
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }
+        else
+        {
+            return new ViewCastInfo(false, (transform.position + DetectionOffset) + dir * ViewRadius, ViewRadius,
+                globalAngle);
+        }
+    }
+
+    public struct EdgeInfo
+    {
+        public Vector3 PointA;
+        public Vector3 PointB;
+
+        public EdgeInfo(Vector3 pointA, Vector3 pointB)
+        {
+            PointA = pointA;
+            PointB = pointB;
+        }
+    }
+
+    public struct ViewCastInfo
+    {
+        public bool Hit;
+        public Vector3 Point;
+        public float Distance;
+        public float Angle;
+
+        public ViewCastInfo(bool hit, Vector3 point, float distance, float angle)
+        {
+            Hit = hit;
+            Point = point;
+            Distance = distance;
+            Angle = angle;
+        }
+    }
+}
